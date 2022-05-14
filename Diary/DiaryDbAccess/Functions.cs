e961c5fa179d221fe52fdac0f1ea375bb06ccf7b
StasAndreev <stas.andreev.172@gmail.com>
2022-05-14 20:31:24 +0300

    Fix argument check for DBAccess Inserts, Add DBAccess updates and deletes

--- Diary/DiaryDbAccess/Functions.cs
+++ Diary/DiaryDbAccess/Functions.cs
@@ -15,6 +15,11 @@ namespace DiaryDbAccess
         /// <returns> id of this user </returns>
         public static int InsertUser(User user)
         {
+            if (user.Login == null || user.Password == null)
+            {
+                throw new ArgumentException("Some of the required fields are missing");
+            }
+
             int result = 0;
             using (DiaryContext db = new DiaryContext())
             {
@@ -32,9 +37,16 @@ namespace DiaryDbAccess
         /// <returns> id of this task </returns>
         public static int InsertTask(Task task)
         {
+            if (task.UserID == null || task.Name == null || task.StartTime == null || task.EndTime == null ||
+                task.TaskTypeID == null || task.RepeatRateID == null)
+            {
+                throw new ArgumentException("Some of the required fields are missing");
+            }
+
             int result = 0;
             using (DiaryContext db = new DiaryContext())
             {
+                task.IsDone = false;
                 Task t = db.Tasks.Add(task);
                 db.SaveChanges();
                 result = t.ID.Value;
@@ -47,12 +59,17 @@ namespace DiaryDbAccess
         /// </summary>
         /// <param name="type"> Task type info </param>
         /// <returns> id for this task </returns>
-        public static int InsertType(TaskType type)
+        public static int InsertTaskType(TaskType taskType)
         {
+            if (taskType.Name == null || taskType.Color == null)
+            {
+                throw new ArgumentException("Some of the required fields are missing");
+            }
+
             int result = 0;
             using (DiaryContext db = new DiaryContext())
             {
-                TaskType tt = db.TaskTypes.Add(type);
+                TaskType tt = db.TaskTypes.Add(taskType);
                 db.SaveChanges();
                 result = tt.ID.Value;
             }
@@ -65,22 +82,109 @@ namespace DiaryDbAccess
         /// <param name="type"> Task type info </param>
         public static void UpdateTask(Task task)
         {
+            using (DiaryContext db = new DiaryContext())
+            {
+                var query =
+                    from t in db.Tasks
+                    where t.ID == task.ID
+                    select t;
 
+                if (query.Count() == 0)
+                {
+                    throw new ArgumentException("Item for update was not found");
+                }
+                else
+                {
+                    Task dbTask = query.First();
+                    dbTask.UserID = task.UserID ?? dbTask.UserID;
+                    dbTask.Name = task.Name ?? dbTask.Name;
+                    dbTask.StartTime = task.StartTime ?? dbTask.StartTime;
+                    dbTask.EndTime = task.EndTime ?? dbTask.EndTime;
+                    dbTask.Note = task.Note ?? dbTask.Note;
+                    dbTask.TaskTypeID = task.TaskTypeID ?? dbTask.TaskTypeID;
+                    dbTask.RepeatRateID = task.RepeatRateID ?? dbTask.RepeatRateID;
+                    dbTask.IsDone = task.IsDone ?? dbTask.IsDone;
+                    db.SaveChanges();
+                }
+            }
         }
 
-        public static void UpdateType(TaskType type)
+        /// <summary>
+        /// Updates existing task type found by ID field
+        /// </summary>
+        /// <param name="type"> Task type info </param>
+        public static void UpdateTaskType(TaskType taskType)
         {
+            using (DiaryContext db = new DiaryContext())
+            {
+                var query =
+                    from tt in db.TaskTypes
+                    where tt.ID == taskType.ID
+                    select tt;
 
+                if (query.Count() == 0)
+                {
+                    throw new ArgumentException("Item for update was not found");
+                }
+                else
+                {
+                    TaskType dbTaskType = query.First();
+                    dbTaskType.UserID = taskType.UserID ?? dbTaskType.UserID;
+                    dbTaskType.Name = taskType.Name ?? dbTaskType.Name;
+                    dbTaskType.Color = taskType.Color ?? dbTaskType.Color;
+                    db.SaveChanges();
+                }
+            }
         }
 
+        /// <summary>
+        /// Deletes existing task found by ID field
+        /// </summary>
+        /// <param name="type"> Task type info </param>
         public static void DeleteTask(int taskId)
         {
+            using (DiaryContext db = new DiaryContext())
+            {
+                var query =
+                    from t in db.Tasks
+                    where t.ID == taskId
+                    select t;
 
+                if (query.Count() == 0)
+                {
+                    throw new ArgumentException("Item for delete was not found");
+                }
+                else
+                {
+                    db.Tasks.Remove(query.First());
+                    db.SaveChanges();
+                }
+            }
         }
 
-        public static void DeleteType(int typeId)
+        /// <summary>
+        /// Updates existing task type found by ID field
+        /// </summary>
+        /// <param name="type"> Task type info </param>
+        public static void DeleteType(int taskTypeId)
         {
+            using (DiaryContext db = new DiaryContext())
+            {
+                var query =
+                    from tt in db.TaskTypes
+                    where tt.ID == taskTypeId
+                    select tt;
 
+                if (query.Count() == 0)
+                {
+                    throw new ArgumentException("Item for delete was not found");
+                }
+                else
+                {
+                    db.TaskTypes.Remove(query.First());
+                    db.SaveChanges();
+                }
+            }
         }
 
         public static int SelectUser(string login, string password)
