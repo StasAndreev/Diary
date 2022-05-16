64d7d6a1660c49853b68c0f0ec2ea191e0e05185
StasAndreev <stas.andreev.172@gmail.com>
2022-05-16 19:09:46 +0300

    Delete WebAPI test project

---
+++ Diary/DiaryWebServer/Controllers/RepeatRateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using DiaryDbAccess;
+
+namespace DiaryWebServer.Controllers
+{
+    public class RepeatRateController : ApiController
+    {
+        public HttpResponseMessage GetRepeatRates()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content
+            return response;
+        }
+    }
+}
